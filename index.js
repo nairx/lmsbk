b799c240cf5dc033cc106c7e7e3b2b09b8d87872
nairx <nairxprv@gmail.com>
2025-06-07 17:58:17 +0530

    Updated

--- index.js
+++ index.js
@@ -3,19 +3,37 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.js";
+import dbConnect from "./lib/mongoose.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+dbConnect()
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.error(err));
+
+const PORT = process.env.PORT || 5000;
+
+  // mongoose
+  // .connect(process.env.MONGO_URI)
+  // .then(() => {
+  //   app.listen(8080, () => {
+  //     console.log("Server Started on port 8080");
+  //   });
+  // })
+  // .catch((error) => {
+  //   console.log(error);
+  // });
+
+
 
 app.use("/api/users", userRoutes);
 
 // app.use("/api/courses", courseRoutes);
 
-const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
